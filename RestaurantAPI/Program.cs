ce21040b078b0e8c19e360d545dde2f2bc84b0c0
Roktiw00 <wkalaga73@gmail.com>
2024-02-27 23:04:25 +0100

    In main program added DbContext and seeder class scope

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,14 +1,16 @@
-using RestaurantAPI.Controllers;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<RestaurantDbContext>();
+builder.Services.AddScoped<RestaurantSeeder>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+seeder.Seed();
+
 app.UseHttpsRedirection();
 
 //app.UseAuthorization();
