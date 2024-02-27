af0f5c45e6034e7b1304c998270a8ae66596b023
Roktiw00 <wkalaga73@gmail.com>
2024-02-27 22:13:35 +0100

    Added first model classes and created db using ef core

---
+++ RestaurantAPI/Entities/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantAPI.Entities
+{
+	public class Address
+	{
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+
+        public Restaurant Restaurant { get; set; }
+    }
+}
---
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace RestaurantAPI.Entities
+{
+	public class RestaurantDbContext : DbContext
+	{
+		private string _connectionString = "Server=DESKTOP-Q5Q2G97;Database=RestaurantDb;Trusted_Connection=true;Encrypt=false;";
+
+		public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
