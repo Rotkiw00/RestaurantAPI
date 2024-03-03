af1b04b83206b2d36157ecb4b0f058e43f08ba16
Roktiw00 <wkalaga73@gmail.com>
2024-03-03 20:31:28 +0100

    Made adjustments to Street and City columns of Address table

--- RestaurantAPI/Entities/RestaurantDbContext.cs
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -20,7 +20,17 @@ namespace RestaurantAPI.Entities
             modelBuilder.Entity<Dish>()
                 .Property(d => d.Name)
                 .IsRequired();
-        }
+
+            modelBuilder.Entity<Address>()
+                .Property(a => a.City)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			modelBuilder.Entity<Address>()
+				.Property(a => a.Street)
+				.IsRequired()
+				.HasMaxLength(50);
+		}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
