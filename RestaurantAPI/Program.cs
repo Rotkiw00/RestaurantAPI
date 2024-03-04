1bf62b772b3913ad3d93dce386928457ed701806
Roktiw00 <wkalaga73@gmail.com>
2024-03-04 23:13:18 +0100

    Deleted line with unnecessary registered controller

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
