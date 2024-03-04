0e211a973c30d3292999500fe499882f826a2eb0
Roktiw00 <wkalaga73@gmail.com>
2024-03-04 23:14:09 +0100

    Added restaurant controller with two GET actions

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantAPI.Controllers
+{
+	[Route("api/restaurant")]
+	public class RestaurantController : ControllerBase
+	{
+		private readonly RestaurantDbContext _dbContext;
+
+		public RestaurantController(RestaurantDbContext dbContext)
+        {
+			_dbContext = dbContext;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Restaurant>> GetAll()
+		{
+			var restaurants = _dbContext
+				.Restaurants
+				.ToList();
+
+			return Ok(restaurants);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<Restaurant> Get([FromRoute]int id)
+		{
+			var restaurant = _dbContext
+				.Restaurants
+				.FirstOrDefault(r => r.Id == id);
+
+			if (restaurant is null) return NotFound();
+
+			return Ok(restaurant);
+		}
+    }
+}
