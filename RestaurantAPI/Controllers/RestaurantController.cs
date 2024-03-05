5b0ce457f07d798666d3005c7061353be52606b7
Roktiw00 <wkalaga73@gmail.com>
2024-03-05 23:05:53 +0100

    Modified restaurant controller due to mapping dto objects

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+using AutoMapper;
 
 namespace RestaurantAPI.Controllers
 {
@@ -6,10 +9,12 @@ namespace RestaurantAPI.Controllers
 	public class RestaurantController : ControllerBase
 	{
 		private readonly RestaurantDbContext _dbContext;
+		private readonly IMapper _mapper;
 
-		public RestaurantController(RestaurantDbContext dbContext)
+		public RestaurantController(RestaurantDbContext dbContext, IMapper mapper)
         {
 			_dbContext = dbContext;
+			_mapper = mapper;
 		}
 
 		[HttpGet]
@@ -17,9 +22,25 @@ namespace RestaurantAPI.Controllers
 		{
 			var restaurants = _dbContext
 				.Restaurants
+				.Include(r => r.Address)
+				.Include(r => r.Dishes)
 				.ToList();
 
-			return Ok(restaurants);
+			/* Wrong way for mapping object to its DTO
+			 better to use AutoMapper
+
+			var restaurantsDto = restaurants.Select(r => new RestaurantDto()
+			{
+				Name = r.Name,
+				Description = r.Description,
+				Category = r.Category,
+				HasDelivery = r.HasDelivery,
+			});
+			
+			 */
+
+			var restaurantsDto = _mapper.Map<List<RestaurantDto>>(restaurants);
+			return Ok(restaurantsDto);
 		}
 
 		[HttpGet("{id}")]
@@ -31,7 +52,8 @@ namespace RestaurantAPI.Controllers
 
 			if (restaurant is null) return NotFound();
 
-			return Ok(restaurant);
+			var restaurantDto = _mapper.Map<RestaurantDto>(restaurant); // <T> is the generic type which is the destination type to return
+			return Ok(restaurantDto);
 		}
     }
 }
