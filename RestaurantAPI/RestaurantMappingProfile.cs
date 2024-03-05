c81ccbf3e8676b1b115f9f88dfd2eb5475fd5144
Roktiw00 <wkalaga73@gmail.com>
2024-03-05 23:02:53 +0100

    Added AutoMapper nuget package and configured profile for it

---
+++ RestaurantAPI/RestaurantMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using RestaurantAPI.Models;
+using AutoMapper;
+
+namespace RestaurantAPI
+{
+	public class RestaurantMappingProfile : Profile
+	{
+        public RestaurantMappingProfile()
+        {
+            CreateMap<Restaurant, RestaurantDto>()
+                .ForMember(d => d.City, s => s.MapFrom(r => r.Address.City))
+                .ForMember(d => d.Street, s => s.MapFrom(r => r.Address.Street))
+                .ForMember(d => d.PostalCode, s => s.MapFrom(r => r.Address.PostalCode));
+
+            CreateMap<Dish, DishDto>();
+		}
+    }
+}
