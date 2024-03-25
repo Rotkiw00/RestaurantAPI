dfadd26b7b9cb9de7b0d9b9a82e8f5dc4f66abed
Roktiw00 <wkalaga73@gmail.com>
2024-03-25 20:32:35 +0100

    Added and registered Swagger as API documentation

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddScoped<RestaurantSeeder>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // this.GetType().Assembly -> 'this' not works because of TopLevelStatement
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -22,6 +23,12 @@ seeder.Seed();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+	s.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API");
+});
 
 //app.UseAuthorization();
 
