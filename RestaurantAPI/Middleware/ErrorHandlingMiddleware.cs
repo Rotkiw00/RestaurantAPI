34e66c2be555fb3327414b178c8ea4e40cb66f9b
Roktiw00 <wkalaga73@gmail.com>
2024-03-25 20:02:20 +0100

    Added error handling middleware and configured it in main program

---
+++ RestaurantAPI/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using RestaurantAPI.Exceptions;
+
+namespace RestaurantAPI.Middleware
+{
+	public class ErrorHandlingMiddleware : IMiddleware
+	{
+		private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+		public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+			_logger = logger;
+		}
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			try
+			{
+				await next.Invoke(context);
+			}
+			catch (NotFoundException notFoundEx)
+			{
+				context.Response.StatusCode = 404;
+				await context.Response.WriteAsync(notFoundEx.Message);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, ex.Message);
+
+				context.Response.StatusCode = 500;
+				await context.Response.WriteAsync("Something went wrong...");
+			}
+		}
+	}
+}
--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,13 +1,18 @@
+using NLog.Web;
+using RestaurantAPI.Middleware;
 using RestaurantAPI.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseNLog();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // this.GetType().Assembly -> 'this' not works because of TopLevelStatement
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 var app = builder.Build();
 
@@ -15,6 +20,7 @@ var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
 seeder.Seed();
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 //app.UseAuthorization();
