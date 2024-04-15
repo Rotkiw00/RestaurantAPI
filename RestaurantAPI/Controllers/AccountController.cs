63ccc8d8e4ea84c4f00b4587647799968ec8737a
Roktiw00 <wkalaga73@gmail.com>
2024-04-15 21:18:45 +0200

    Created login and register actions. Configured jwt settings in json settings configuration. Added new validators and custom exception

---
+++ RestaurantAPI/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+
+namespace RestaurantAPI.Controllers
+{
+	[Route("api/account")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		private readonly IAccountService _service;
+        public AccountController(IAccountService service)
+		{
+			_service = service;
+		}
+
+		[HttpPost("register")]
+		public ActionResult RegisterUser([FromBody] RegisterUserDto userDto)
+		{
+			_service.RegisterUser(userDto);
+			return Ok();
+		}
+
+		[HttpPost("login")]
+		public ActionResult LoginUser([FromBody]LoginUserDto userDto)
+		{
+			string tokenJwt = _service.GetJwtToken(userDto);
+			return Ok(tokenJwt);
+		}
+	}
+}
---
+++ RestaurantAPI/Models/LoginUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantAPI.Models
+{
+	public class LoginUserDto
+	{
+        public string Email { get; set; }
+		public string Password { get; set; }
+    }
+}
