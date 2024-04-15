b594d396624095c71ede72a6e8ca122182008112
Roktiw00 <wkalaga73@gmail.com>
2024-04-15 21:20:09 +0200

    Implemented appsettings for jwt configuration and managed changes in main program (added services)

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,20 +1,60 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using NLog.Web;
 using RestaurantAPI.Middleware;
+using RestaurantAPI.Models;
+using RestaurantAPI.Models.Validators;
 using RestaurantAPI.Services;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseNLog();
 
+
+AuthenticationSettings authenticationSettings = new();
+builder.Services.AddSingleton(authenticationSettings);
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+builder.Services.AddAuthentication(opt =>
+{
+	opt.DefaultAuthenticateScheme = "Bearer";
+	opt.DefaultScheme = "Bearer";
+	opt.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+	cfg.RequireHttpsMetadata = false;
+	cfg.SaveToken = true;
+	cfg.TokenValidationParameters = new()
+	{
+		ValidIssuer = authenticationSettings.JwtIssuer,
+		ValidAudience = authenticationSettings.JwtIssuer,
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+	};
+});
+
 builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+
 builder.Services.AddDbContext<RestaurantDbContext>();
+
 builder.Services.AddScoped<RestaurantSeeder>();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // this.GetType().Assembly -> 'this' not works because of TopLevelStatement
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+// this.GetType().Assembly -> 'this' not works because of TopLevelStatement
+
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
 builder.Services.AddScoped<IDishService, DishService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
 builder.Services.AddScoped<RequestTimeMiddleware>();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -32,8 +72,6 @@ app.UseSwaggerUI(s =>
 {
 	s.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API");
 });
-
-//app.UseAuthorization();
 
 app.MapControllers();
 
